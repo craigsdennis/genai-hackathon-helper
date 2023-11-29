6c7023f6f96cce2f0264fcba9a7e148a4db1adb1
Craig Dennis <cdennis@twilio.com>
2023-11-29 00:37:27 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Hackathon Helper
+
+This repository is a starter kit for an OpenAI hackathon. It uses [Streamlit](https://streamlit.io) to allow for exploration/note-taking.
+
+## Installation
+
+Copy `.env.example` to `.env`
+Update with your missing keys
+
+```bash
+python -m venv venv
+source ./venv/bin/activate
+python -m pip install -r requirements.txt
+```
+
+## Run Streamlit Apps🔥
+
+### Introduction to OpenAI API (Part 1)
+
+```bash
+python -m streamlit run intro.py
+```
+
+### HackGPT
+
+```bash
+python -m streamlit run hackgpt.py
+```
+
---
+++ utils.py
@@ -0,0 +1,15 @@
+import inspect
+import json
+
+
+def get_function_body(fn, replacements=None):
+    source_lines = inspect.getsourcelines(fn)[0]
+    # skip the def and return (add options if we end up not using this)
+    body = "".join(source_lines[1:-1])
+    if replacements is not None:
+        for key, value in replacements.items():
+            # The JSON dump of the value will quote/escape things properly
+            body = body.replace(key, json.dumps(value))
+    return body
+
+email_re = r"[^\.\s@:](?:[^\s@:]*[^\s@:\.])?@[^\.\s@]+(?:\.[^\.\s@]+)*"
